db9593608dca87ee9a298e695ede96aaebdf5a65
Sanjyanaa <82484235+sanjyanaa@users.noreply.github.com>
2022-11-11 17:03:45 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# colour = input("Enter a colour")
+# plural_noun = input("Enter a plural_noun")
+# celebrity = input("Enter a celebrity")
+#
+#
+#
+# print("Roses are " + colour)
+# print(plural_noun + " are blue")
+# print("I love " + celebrity)
+
+boyfriend_one = input("Enter the name of your first boyfriend:")
+reason = input("What was the reason you broke up:")
+second_boy = input("Who was your next boyfriend:")
+laugh = input("enter a verb that you do:")
+married = input("Name a crazy thing you almost did:")
+thankful = input("Type thankful :")
+thanks_to = input("Who would you say thanks to:")
+angel = input("Why would you say thanks to him:")
+
+print("Thought I'd end up with " + boyfriend_one)
+print("But he  " + reason )
+print("Wrote some songs about " + second_boy )
+print("Now I listen and " + laugh )
+print("Even almost got " + married)
+print("And for Pete, " + thankful )
+print("Wish I could say thank you to " + thanks_to)
+print("cause " + angel )
+
+
